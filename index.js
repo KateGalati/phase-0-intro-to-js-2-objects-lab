9d5a5103481c47a9a6794ae0f04374e3e3140bd8
Kate Galati <kategalati@Kates-MacBook-Air.local>
2022-08-31 17:31:25 -0700

    commit tree 92e30dcb1867b132f6c8ba1973c1a031c15ea5ee

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kate",
+    streetAddress: "8448 SW 68th St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = {...employee};
+
+  newEmployee[key] = value;
+
+  return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  
+    let newEmployee = {...delete employee.key};
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
